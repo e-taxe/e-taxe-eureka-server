7e38146c44e40ad3559ef6929acb1b5261e60ce6
adilmax <ait.ahmed.adil.mx@gmail.com>
2021-11-15 15:36:16 +0100

    init eureka server

---
+++ src/test/java/com/irisi/etaxeeurekaserver/ETaxeEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irisi.etaxeeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETaxeEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
